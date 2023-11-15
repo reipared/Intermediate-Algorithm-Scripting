81b47fbeaedd6c84f2f40d2a64105f43f28d3bc4
Reinaldo <reinaldopared@gmail.com>
2023-11-15 10:54:19 -0400

    Finish exercise

---
+++ Sum_All_Numbers_in_a_Range.js
@@ -0,0 +1,20 @@
+function sumAll(arr) {
+	// Find the minimum and maximum values in the array.
+	var min = Math.min(arr[0], arr[1]);
+	var max = Math.max(arr[0], arr[1]);
+
+	// Initialize the sum
+	var sum = 0;
+
+	// Iterate through the range and add each number to the sum.
+	for (var i = min; i <= max; i++) {
+		sum += 1;
+	}
+
+	// Return the final sum.
+	return sum;
+}
+
+// Example usage
+var result = sumAll([1, 4]);
+console.log(result); // Output 10
