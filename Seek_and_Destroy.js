5a075f90386e7c28c2f2c2a0a26e1138ce5001bb
Reinaldo <reinaldopared@gmail.com>
2023-11-15 11:13:44 -0400

    Update

--- Seek_and_Destroy.js
+++ Seek_and_Destroy.js
@@ -1,4 +1,14 @@
 function destroyer(arr) {
-	// Convert the arguments object to an array, excluding the first element (arr)
-	var args = Array.from(arguments);
+	// Concatenate the two arrays into a single array
+	var combinedArray = arr1.concat(arr2);
+
+	// Use filter to create a new array with only the elements that are unique
+	var diff = combinedArray.filter(function (item) {
+		// Check if the item is not present in both arrays
+		return arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1;
+	});
+
+	return diff;
 }
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
