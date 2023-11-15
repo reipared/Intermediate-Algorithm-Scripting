4ed1fb257d7e7c3094b74e6fa9df4615beb4e3fc
Reinaldo <reinaldopared@gmail.com>
2023-11-15 11:03:29 -0400

    Update Diff_Two_Arrays.js

---
+++ Diff_Two_Arrays.js
@@ -0,0 +1,19 @@
+function diffArray(arr1, arr2) {
+	// Concatenate the two arrays into a single array
+	var combinedArray = arr1.concat(arr2);
+
+	// Use filter to create a new array with only the elements that are unique
+	var diff = combinedArray.filter(function (item) {
+		// Check if the item is not present in both arrays
+		return arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1;
+	});
+
+	return diff;
+}
+
+// Example:
+var array1 = [1, 2, 3, 5];
+var array2 = [1, 2, 3, 4, 5];
+
+var result = diffArray(array1, array2);
+console.log(result); // Output: [4]
