a4e168f2ac9343b8176206b45ff198136656bb5f
Reinaldo <reinaldopared@gmail.com>
2023-11-16 10:32:32 -0400

    Update

--- sortedUnion.js
+++ sortedUnion.js
@@ -1,20 +1,25 @@
 function uniteUnique() {
-  // Convert the arguments object to an array
-  var args = Array.from(arguments);
+	// Convert the arguments object to an array
+	var args = Array.from(arguments);
 
-  // Combine all the arrays into a single array
-  var combinedArray = args.reduce(function(arr1, arr2) {
-    // Use a Set to keep track of unique elements
-    var uniqueSet = new Set(arr1);
+	// Combine all the arrays into a single array
+	var combinedArray = args.reduce(function (arr1, arr2) {
+		// Use a Set to keep track of unique elements
+		var uniqueSet = new Set(arr1);
 
-    // Filter out elements that already exist in the set
-    var filteredArr2.forEach(function(item) {
-      uniqueSet.add(item);
-    });
+		// Filter out elements that already exists in the set
+		var filteredArr2 = arr2.filter(function (item) {
+			return !uniqueSet.has(item);
+		});
 
-    // Convert the set back to an array
-    return Array.from(uniqueSet);
-  }, []);
+		// Add the filtered elements to the set
+		filteredArr2.forEach(function (item) {
+			uniqueSet.add(item);
+		});
 
-  return combinedArray;
+		// Convert the set back to an array
+		return Array.from(uniqueSet);
+	}, []);
+
+	return combinedArray;
 }
