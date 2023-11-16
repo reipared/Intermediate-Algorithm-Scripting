4ca74c33d2ae346f0b1af459f5acc998e8170ad1
Reinaldo <reinaldopared@gmail.com>
2023-11-16 11:34:02 -0400

    Example Added

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -10,3 +10,39 @@ function whereforeArtThou(collection, source) {
 		return true;
 	});
 }
+
+// Example 1
+var result1 = whereforeArtThou(
+	[
+		{ name: "John", age: 30, city: "New York" },
+		{ name: "Jane", age: 25, city: "San Francisco" },
+		{ name: "Bob", age: 40, city: "Los Angeles" },
+	],
+	{ age: 25, city: "San Francisco" }
+);
+console.log(result1);
+// Output: [ { name: 'Jane', age: 25, city: 'San Francisco' } ]
+
+// Example 2
+var result2 = whereforeArtThou(
+	[
+		{ brand: "Apple", model: "iPhone", color: "Silver" },
+		{ brand: "Samsung", model: "Galaxy", color: "Black" },
+		{ brand: "Google", model: "Pixel", color: "White" },
+	],
+	{ model: "iPhone", color: "Silver" }
+);
+console.log(result2);
+// Output: [ { brand: 'Apple', model: 'iPhone', color: 'Silver' } ]
+
+// Example 3
+var result3 = whereforeArtThou(
+	[
+		{ name: "Alice", occupation: "Engineer", location: "Seattle" },
+		{ name: "Bob", occupation: "Teacher", location: "New York" },
+		{ name: "Charlie", occupation: "Doctor", location: "Los Angeles" },
+	],
+	{ occupation: "Doctor", location: "Los Angeles" }
+);
+console.log(result3);
+// Output: [ { name: 'Charlie', occupation: 'Doctor', location: 'Los Angeles' } ]
